cccca34002ebd1a1e9bad6075f46ca008000b057
Maryna-Hemb <marinagembarskaa@gmail.com>
2023-03-30 20:31:03 +0300

    delet prev from app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import { GlobalStyle } from './GlobalStyle';
 import { Layout } from './Layout.styled';
 
@@ -7,60 +6,23 @@ import { ContactList } from './contactList/ContactList';
 import { Filter } from './filter/Filter';
 import { TitleH1, TitleH2 } from './App.styled';
 
-const getInitialContacts = () => {
-  const savedContacts = localStorage.getItem('contacts');
-  if (savedContacts !== null) {
-    return JSON.parse(savedContacts);
-  }
-  return [];
-};
+// const getInitialContacts = () => {
+//   const savedContacts = localStorage.getItem('contacts');
+//   if (savedContacts !== null) {
+//     return JSON.parse(savedContacts);
+//   }
+//   return [];
+// };
 
 export const App = () => {
-  const [contacts, setContacts] = useState(getInitialContacts);
-  const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  const addContact = newContactValues => {
-    let nameRepeat = false;
-    contacts.forEach(name => {
-      if (name.name.toLowerCase() === newContactValues.name.toLowerCase()) {
-        nameRepeat = true;
-        return alert(`${newContactValues.name} is already in contacts`);
-      }
-    });
-
-    if (nameRepeat) {
-      return;
-    }
-
-    setContacts(prevState => [...prevState, newContactValues]);
-  };
-
-  const deleteContact = contactId => {
-    setContacts(prevState =>
-      prevState.filter(contact => contact.id !== contactId)
-    );
-  };
-
-  const changeFilter = event => {
-    setFilter(event.target.value.trim());
-  };
-
   return (
     <Layout>
       <TitleH1>Phonebook</TitleH1>
-      <ContactForm onAddNewContact={addContact} />
+      <ContactForm />
 
       <TitleH2>Contacts</TitleH2>
-      <Filter filter={filter} onFilterChange={changeFilter} />
-      <ContactList
-        contacts={contacts}
-        namefilter={filter}
-        onDeleteContact={deleteContact}
-      />
+      <Filter />
+      <ContactList />
       <GlobalStyle />
     </Layout>
   );
